6eb6fea357c996f186fe189af0f96aab3e9d1e2e
Yi <differentialmanifold@gmail.com>
2016-07-05 08:11:41 +0800

    muti selection

--- plugins/slick.treemovemanager.js
+++ plugins/slick.treemovemanager.js
@@ -11,12 +11,11 @@
         var _dataView;
         var _canvas;
         var _dragging;
-        var _extend;
+        var _extendList;
         var _id;
         var _parentId;
         var _indent;
         var _collapsed;
-        var _originItemId = null;
         var _self = this;
         var _handler = new Slick.EventHandler();
         var _defaults = {
@@ -97,27 +96,50 @@
             return itemsToDrag;
         }
 
-        function isParent(item1, item2) {
+        function isParent(item1, item2, item) {
             var parent1 = _dataView.getItemById(item1[_parentId]);
             if (!parent1) {
                 return false;
             }
+
             if (parent1[_id] === item2[_id]) {
                 return true;
             }
 
-            return isParent(parent1, item2);
+            if (!item) {
+                item = item1;
+            }
+
+            if (parent1[_id] === item[_id]) {
+                throw new Error("prevent dead cycle, id is " + item[_id]);
+            }
+
+            return isParent(parent1, item2, item);
         }
 
-        function moveItem(targetItem, dragItem, moveType) {
-            var dragItemList = getItemsToDrag(dragItem);
-            var targetItemList = getItemsToDrag(targetItem);
-            var dragParent = _dataView.getItemById(dragItem[_parentId]);
+        function setParentId(itemList, parentId) {
+            for (var i = 0; i < itemList.length; i++) {
+                itemList[i][_parentId] = parentId;
+            }
+        }
+
+        function moveItem(targetItem, dragItemList, moveType) {
+            var dragParent = _dataView.getItemById(dragItemList[0][_parentId]);
+
+            var i, dragItem, dragItems = [],
+                targetItems;
+
+            targetItems = getItemsToDrag(targetItem);
+            dragItem = dragItemList[0];
+
+            for (i = 0; i < dragItemList.length; i++) {
+                dragItems = dragItems.concat(getItemsToDrag(dragItemList[i]));
+            }
 
             _dataView.beginUpdate();
-            var i;
-            for (i = 0; i < dragItemList.length; i++) {
-                _dataView.deleteItem(dragItemList[i][_id]);
+
+            for (i = 0; i < dragItems.length; i++) {
+                _dataView.deleteItem(dragItems[i][_id]);
             }
 
             var diff = targetItem[_indent] - dragItem[_indent];
@@ -127,37 +149,92 @@
             var positionToInsert;
             if (moveType === 'prev') {
                 positionToInsert = idx;
-                dragItem[_parentId] = targetItem[_parentId];
+                setParentId(dragItemList, targetItem[_parentId]);
             } else if (moveType === 'inner') {
                 if (isParent(dragItem, targetItem)) {
-                    positionToInsert = idx + targetItemList.length - dragItemList.length;
+                    positionToInsert = idx + targetItems.length - dragItems.length;
                 } else {
-                    positionToInsert = idx + targetItemList.length;
+                    positionToInsert = idx + targetItems.length;
                 }
                 diff = diff + 1;
-                dragItem[_parentId] = targetItem[_id];
+                setParentId(dragItemList, targetItem[_id]);
             } else if (moveType === 'next') {
                 if (isParent(dragItem, targetItem)) {
-                    positionToInsert = idx + targetItemList.length - dragItemList.length;
+                    positionToInsert = idx + targetItems.length - dragItems.length;
                 } else {
-                    positionToInsert = idx + targetItemList.length;
+                    positionToInsert = idx + targetItems.length;
                 }
-                dragItem[_parentId] = targetItem[_parentId];
+                setParentId(dragItemList, targetItem[_parentId]);
+            }
+
+            for (i = 0; i < dragItems.length; i++) {
+                dragItems[i][_indent] = dragItems[i][_indent] + diff;
+                _dataView.insertItem(positionToInsert + i, dragItems[i]);
             }
 
             for (i = 0; i < dragItemList.length; i++) {
-                dragItemList[i][_indent] = dragItemList[i][_indent] + diff;
-                _dataView.insertItem(positionToInsert + i, dragItemList[i]);
+                _dataView.updateItem(dragItemList[i][_id], dragItemList[i]);
             }
 
-            _dataView.updateItem(targetItem[_id], targetItem);
-            _dataView.updateItem(dragItem[_id], dragItem);
 
+            _dataView.updateItem(targetItem[_id], targetItem);
             if (dragParent && dragParent[_id]) {
                 _dataView.updateItem(dragParent[_id], dragParent);
             }
 
             _dataView.endUpdate();
+        }
+
+        function getItemsFromRows(rows) {
+            var items = [];
+            var item;
+            for (var i = 0; i < rows.length; i++) {
+                item = _dataView.getItem(rows[i]);
+                items.push(item);
+            }
+            return items;
+        }
+
+        function isItemsSameParent(items) {
+            for (var i = 0; i < items.length - 1; i++) {
+                if (items[i][_parentId] !== items[i + 1][_parentId]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        function closeBeforeDrag(dragItemList) {
+            _extendList = [];
+            var item, extendItem;
+
+            _dataView.beginUpdate();
+            for (var i = 0; i < dragItemList.length; i++) {
+                item = dragItemList[i];
+                if (item) {
+                    if (!item[_collapsed]) {
+                        extendItem = {};
+                        extendItem.id = item[_id];
+                        _extendList.push(extendItem);
+                    }
+                    item[_collapsed] = true;
+                    _dataView.updateItem(item[_id], item);
+                }
+            }
+            _dataView.endUpdate();
+        }
+
+        function openAfterDrop() {
+            var item;
+            if (_extendList.length > 0) {
+                _dataView.beginUpdate();
+                for (var i = 0; i < _extendList.length; i++) {
+                    item = _dataView.getItemById(_extendList[i].id);
+                    item[_collapsed] = false;
+                    _dataView.updateItem(item[_id], item);
+                }
+                _dataView.endUpdate();
+            }
         }
 
         function handleDragStart(e, dd) {
@@ -171,53 +248,71 @@
                 return false;
             }
 
-            var item = _dataView.getItem(cell.row);
-            var dragItemList = getItemsToDrag(item);
+            var selectedRows = _grid.getSelectedRows();
 
-            if (apply(options.beforeDrag, [item, dragItemList], !!options.beforeDrag) === false) {
+            selectedRows.sort();
+
+            var dragItemList = getItemsFromRows(selectedRows);
+
+            if (selectedRows.length == 0 || $.inArray(cell.row, selectedRows) == -1 || !isItemsSameParent(dragItemList)) {
+                selectedRows = [cell.row];
+                dragItemList = [_dataView.getItem(cell.row)];
+                _grid.setSelectedRows(selectedRows);
+            }
+            dd.dragItemList = dragItemList;
+
+            // var item = _dataView.getItem(cell.row);
+            // var dragItemList = getItemsToDrag(item);
+
+            if (apply(options.beforeDrag, [dragItemList], !!options.beforeDrag) === false) {
                 return false;
             }
 
-            dd.dragItem = item;
-            dd.dragItemList = dragItemList;
+            // dd.dragItem = item;
+            // dd.dragItemList = dragItemList;
 
             _dragging = true;
             e.stopImmediatePropagation();
 
-            if (item) {
-                if (!item[_collapsed]) {
-                    _extend = true;
-                    _originItemId = item[_id];
-                }
-                item[_collapsed] = true;
-                _dataView.updateItem(item[_id], item);
-            }
+            closeBeforeDrag(dragItemList);
 
 
-            // var selectedRows = _grid.getSelectedRows();
-
-            // if (selectedRows.length == 0 || $.inArray(cell.row, selectedRows) == -1) {
-            //     selectedRows = [cell.row];
-            //     _grid.setSelectedRows(selectedRows);
+            // if (item) {
+            //     if (!item[_collapsed]) {
+            //         _extend = true;
+            //         _originItemId = item[_id];
+            //     }
+            //     item[_collapsed] = true;
+            //     _dataView.updateItem(item[_id], item);
             // }
-            // dd.selectedRows = selectedRows;
+
+
 
             var rowHeight = _grid.getOptions().rowHeight;
 
-            dd.selectionProxy = $("<div class='slick-reorder-proxy'/>")
+            dd.selectionProxy = $("<ul class='slick-reorder-proxy'/>")
                 .css("position", "absolute")
                 .css("zIndex", "99999")
                 .css("width", $(_canvas).innerWidth())
-                // .css("height", rowHeight * selectedRows.length)
-                .css("height", rowHeight)
+                .css("height", rowHeight * selectedRows.length)
                 .css("background-color", "#cfcfcf")
                 .css("border", "1px #00B83F dotted")
                 .css("opacity", 0.8)
                 .css("filter", "alpha(opacity=80)")
                 .appendTo(_canvas);
 
-            var cellNode = _grid.getCellNode(cell.row, cell.cell);
-            dd.selectionProxy.append($(cellNode).clone().css("background-color", "#cfcfcf"));
+            var selectionLi, cellNode;
+            for (var i = 0; i < selectedRows.length; i++) {
+                selectionLi = $("<li class='slick-reorder-li'/>")
+                    .appendTo(dd.selectionProxy);
+
+                cellNode = _grid.getCellNode(selectedRows[i], cell.cell);
+                $(cellNode).clone()
+                    .css("background-color", "#cfcfcf")
+                    .appendTo(selectionLi);
+            }
+            // var cellNode = _grid.getCellNode(cell.row, cell.cell);
+            // dd.selectionProxy.append($(cellNode).clone().css("background-color", "#cfcfcf"));
 
             dd.guide = $("<div class='slick-reorder-guide'/>")
                 .css("position", "absolute")
@@ -257,30 +352,37 @@
                 moveType = 'next';
             }
 
-
-
             if (targetRow !== dd.targetRow || moveType !== dd.moveType) {
                 var canmove = true,
                     guideTop = -1000,
                     guideHeight = $('.slick-reorder-guide').height();
 
-                if (!targetItem || dd.dragItem[_id] === targetItem[_id]) {
+                if (!targetItem) {
                     canmove = false;
                 }
 
                 if (canmove) {
-                    if (moveType === 'prev' && apply(options.prev, [dd.dragItem, dd.dragItemList, targetItem], !!options.prev) !== false) {
+                    for (var i = 0; i < dd.dragItemList.length; i++) {
+                        if (dd.dragItemList[i][_id] === targetItem[_id]) {
+                            canmove = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (canmove) {
+                    if (moveType === 'prev' && apply(options.prev, [dd.dragItemList, targetItem], !!options.prev) !== false) {
                         guideTop = targetRow * rowHeight + 0.1 * rowHeight;
-                    } else if (moveType === 'inner' && dd.dragItem[_parentId] !== targetItem[_id] && apply(options.inner, [dd.dragItem, dd.dragItemList, targetItem], !!options.inner) !== false) {
+                    } else if (moveType === 'inner' && dd.dragItemList[0][_parentId] !== targetItem[_id] && apply(options.inner, [dd.dragItemList, targetItem], !!options.inner) !== false) {
                         guideTop = targetRow * rowHeight + 0.5 * rowHeight;
-                    } else if (moveType === 'next' && apply(options.next, [dd.dragItem, dd.dragItemList, targetItem], !!options.next) !== false) {
+                    } else if (moveType === 'next' && apply(options.next, [dd.dragItemList, targetItem], !!options.next) !== false) {
                         guideTop = targetRow * rowHeight + 0.9 * rowHeight - guideHeight;
                     } else {
                         canmove = false;
                     }
                 }
 
-                if (!canmove || apply(options.onDrag, [dd.dragItem, dd.dragItemList, targetItem, moveType], !!options.onDrag) === false) {
+                if (!canmove || apply(options.onDrag, [dd.dragItemList, targetItem, moveType], !!options.onDrag) === false) {
                     dd.guide.css("top", -1000);
                     dd.canMove = false;
                 } else {
@@ -304,16 +406,18 @@
 
             dd.guide.remove();
             dd.selectionProxy.remove();
-            if (dd.canMove && apply(options.beforeDrop, [dd.dragItem, dd.dragItemList, dd.targetItem, dd.moveType], !!options.beforeDrop) !== false) {
-                moveItem(dd.targetItem, dd.dragItem, dd.moveType);
-                apply(options.onDrop, [dd.dragItem, dd.dragItemList, dd.targetItem, dd.moveType]);
+            if (dd.canMove && apply(options.beforeDrop, [dd.dragItemList, dd.targetItem, dd.moveType], !!options.beforeDrop) !== false) {
+                moveItem(dd.targetItem, dd.dragItemList, dd.moveType);
+                apply(options.onDrop, [dd.dragItemList, dd.targetItem, dd.moveType]);
             }
-            if (_extend) {
-                var originItem = _dataView.getItemById(_originItemId);
-                originItem[_collapsed] = false;
-                _dataView.updateItem(_originItemId, originItem);
-                _extend = false;
-            }
+
+            openAfterDrop();
+            // if (_extend) {
+            //     var originItem = _dataView.getItemById(_originItemId);
+            //     originItem[_collapsed] = false;
+            //     _dataView.updateItem(_originItemId, originItem);
+            //     _extend = false;
+            // }
         }
 
         $.extend(this, {
